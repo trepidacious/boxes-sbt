3d3d98a536e29aa3c5f5e95da421b7b436e95d96
trepidacious <trepidacious@gmail.com>
2013-12-30 20:48:34 +0000

    Simplified AjaxPassView
    Removed all modes except editing, since signup and reset are now
    performed (much better) by dedicated forms.

--- liftdemo/src/main/scala/boxes/lift/comet/view/AjaxPassView.scala
+++ liftdemo/src/main/scala/boxes/lift/comet/view/AjaxPassView.scala
@@ -18,25 +18,7 @@ import net.liftweb.util.Helpers.strToSuperArrowAssoc
 import SHtml._
 
 
-sealed trait PassViewMode
-
-/**
- * Supports creating a password if we have none, or editing any existing password, repeatedly if required
- */
-case object PassEditing extends PassViewMode
-
-/**
- * Supports only creating a password, once the password is created a message is displayed stating this has been successful
- */
-case object PassCreation extends PassViewMode
-
-/**
- * Supports only resetting the password. The existing password is not required. Once the password is reset a message is
- * displayed stating this has been successful
- */
-case object PassReset extends PassViewMode
-
-class AjaxPassView(user: Ref[User], mode: PassViewMode) extends AjaxView with Loggable {
+class AjaxPassView(user: Ref[User]) extends AjaxView with Loggable {
   var oldPass: Option[String] = None
   var newPassA: Option[String] = None
   var newPassB: Option[String] = None
@@ -47,35 +29,20 @@ class AjaxPassView(user: Ref[User], mode: PassViewMode) extends AjaxView with Lo
   def formSubmit() {
     boxes.Box.transact{
       for {
-        ph <- if (mode == PassEditing) user().passHash() else Some(PassHash(""))
-        old <- if (mode == PassEditing) oldPass else Some("")
+        ph <- user().passHash()
+        old <- oldPass
         a <- newPassA
         b <- newPassB
       } {
         if (a != b) {
-          S.error("New password and repeat do not match.")
-        } else if (mode==PassEditing && !ph.checkPass(old)) {
-          S.error("Incorrect current password.")
-        } else if (mode==PassCreation && !user().passHash().isEmpty) {
-          S.error("Password is already set.")
+          S.error(S.?("user.change.passwords.mismatch"))
+        } else if (!ph.checkPass(old)) {
+          S.error(S.?("user.change.current.incorrect"))
         } else if (User.validatePassword(a).isDefined) {
           S.error(User.validatePassword(a).getOrElse("Invalid new password."))
         } else {
           user().passHash() = Some(PassHash(a))
-          mode match {
-            case PassCreation => S.notice("Password created.")
-            case PassEditing => S.notice("Password changed.")
-            case PassReset => {
-              //Each reset token can only be used once
-              user().clearResetPasswordToken()
-              //User email is validated now, since it must have been used to trigger reset
-              user().validated() = true
-              user().clearValidationToken()              
-              S.notice("Password reset, you are now logged in.")
-//              //Log them in
-//              User.logInFreshSession(user())
-            }
-          }
+          S.notice(S.?("user.password.changed"))
         }
       }      
     }
@@ -90,11 +57,11 @@ class AjaxPassView(user: Ref[User], mode: PassViewMode) extends AjaxView with Lo
   
   override def partialUpdates = List(() => Replace(id, render))
  
-  def renderForm() = 
+  def render = 
     AjaxView.formWithId(
-      (if (mode == PassEditing) passwordLine(S.?("user.password.current"), s=>oldPass=Some(s)) else Nil) ++
-      passwordLine(S.?("user.password.a"),     s=>newPassA=Some(s)) ++
-      passwordLine(S.?("user.password.b"),  s=>newPassB=Some(s)) ++
+      passwordLine(S.?("user.password.current"),  s=>oldPass=Some(s)) ++
+      passwordLine(S.?("user.password.a"),        s=>newPassA=Some(s)) ++
+      passwordLine(S.?("user.password.b"),        s=>newPassB=Some(s)) ++
       (
         ("#label" #> ("")) & 
         ("#control" #> ajaxSubmit(S.?("user.password.change.button"), ()=>formSubmit(), "class" -> "btn btn-primary"))
@@ -108,15 +75,8 @@ class AjaxPassView(user: Ref[User], mode: PassViewMode) extends AjaxView with Lo
                  ("#control" #> m)
                ).apply(AjaxView.formRowXML)}</div>
   
-  def render = {
-    mode match {
-      case PassCreation => if (user().passHash().isDefined) renderMessage(S.?("user.password.set")) else renderForm()
-      case PassEditing => renderForm()
-      case PassReset => renderForm()  //TODO after the user has reset the password, we should really prevent further editing, and if possible log the user in at "/"
-    }
-  }
 }
 
 object AjaxPassView {
-  def apply(user: Ref[User], mode: PassViewMode = PassEditing) = new AjaxPassView(user, mode)
+  def apply(user: Ref[User]) = new AjaxPassView(user)
 }
