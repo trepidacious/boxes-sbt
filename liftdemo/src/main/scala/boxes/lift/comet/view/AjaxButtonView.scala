da5a819e1b8278f6384cd82b449b66157a447f10
trepidacious <trepidacious@gmail.com>
2013-12-30 20:39:21 +0000

    Removed FIXME to support enable/disable of AjaxButtonView
    No obvious good approach to disabling the button, we will just leave it
    appearing enabled, but doing nothing when clicked when logically
    disabled.

--- liftdemo/src/main/scala/boxes/lift/comet/view/AjaxButtonView.scala
+++ liftdemo/src/main/scala/boxes/lift/comet/view/AjaxButtonView.scala
@@ -10,7 +10,9 @@ import net.liftweb.http.js.JsCmds.Noop
 import net.liftweb.http.js.JsCmds.Replace
 import net.liftweb.util.Helpers.strToSuperArrowAssoc
 import boxes.Box
-
+import net.liftweb.http.SHtml.ElemAttr
+import net.liftweb.http.js.JsCmds.SetElemById
+import net.liftweb.http.js.JE
 sealed trait ButtonClass
 
 case object DefaultButton extends ButtonClass
@@ -34,11 +36,10 @@ object AjaxButtonView {
   }
 }
 
-//FIXME implement "enabled"
 class AjaxButtonView(label: Ref[NodeSeq], enabled: Ref[Boolean], action: => Unit, buttonClass: ButtonClass) extends AjaxView {
   lazy val id = "button_" + net.liftweb.util.Helpers.nextFuncName
 
-  //Make sure that we only evern execute the action while we are sure the button should be enabled.
+  //Make sure that we only ever execute the action while we are sure the button should be enabled.
   def onClick() {
     Box.transact{
       if (enabled()) {
@@ -47,11 +48,22 @@ class AjaxButtonView(label: Ref[NodeSeq], enabled: Ref[Boolean], action: => Unit
     }
   }
   
+  val enabledAttr: List[ElemAttr] = List("class" -> ("btn " + AjaxButtonView.bootstrapClass(buttonClass)))
+  val disabledAttr: List[ElemAttr] = (("disabled" -> "disabled"): ElemAttr) +: enabledAttr
+  
   def render = {
+    //If we actually disable the button, it can cause problems since there may be a delay enabling the button
+    //when it is valid. E.g. we might enable it when all input is correct, but this might only occur when the
+    //last input box loses focus and sends the new data, then the browser receives the partial update some time later.
+    //In the meantime the user could well have tried to click the button and failed. Better to just make the button
+    //fail to do anything when clicked, as it does now.
+    //(if (enabled()) enabledAttr else disabledAttr):_*)}
     <div id={id} class="form-horizontal">
-      {ajaxButton(label(), () => {onClick; Noop}, "class" -> ("btn " + AjaxButtonView.bootstrapClass(buttonClass)))}
+      {ajaxButton(label(), () => {onClick; Noop}, enabledAttr:_*)} 
     </div>
   }
 
-  override def partialUpdates = List(() => Replace(id, render))
+  override def partialUpdates = List(
+    () => Replace(id, render)
+  )
 }
