b0c1f2271517e7ff7271fe561707c73a008c8cbb
trepidacious <trepidacious@gmail.com>
2014-02-09 11:35:28 +0000

    Organised imports

--- liftdemo/src/main/scala/boxes/lift/comet/view/AjaxDataLinkView.scala
+++ liftdemo/src/main/scala/boxes/lift/comet/view/AjaxDataLinkView.scala
@@ -1,48 +1,16 @@
 package boxes.lift.comet
 
-import net.liftweb._
-import http._
-import SHtml._
-import net.liftweb.common._
-import net.liftweb.common.Box._
-import net.liftweb.util._
-import net.liftweb.actor._
-import net.liftweb.util.Helpers._
-import net.liftweb.http.js.JsCmds.{SetHtml}
-import boxes.View
-import scala.xml.Text
-import boxes.Var
-import boxes.Path
-import boxes.Val
-import boxes.Ref
-import scala.xml.NodeSeq
-import boxes.BoxImplicits._
-import boxes.Cal
-import boxes.util.NumericClass
-import boxes.persistence.mongo.MongoBox
-import boxes.persistence.ClassAliases
-import com.mongodb.casbah.commons.Imports._
-import boxes.list.ListRef
-import boxes.lift.user.PassHash
-import boxes.Box
-import net.liftweb.http.js.JsCmds._
-import net.liftweb.common.Loggable
-import net.liftweb.http.js.JsCmd
-import boxes.Reaction
-import net.liftweb.http.js.JE
 import scala.language.implicitConversions
-import boxes.lift.user.User
-import net.liftweb.http.js.JsCmds
-import boxes.lift.comet.view.AjaxButtonView
-import org.joda.time.DateTime
-import org.joda.time.format.DateTimeFormat
-import org.joda.time.format.DateTimeFormatter
-import org.joda.time.LocalTime
-import net.liftweb.http.js._
-import net.liftweb.json._
-import net.liftweb.json.JsonAST._
-import net.liftweb.json.JsonDSL._
 import scala.util.Try
+
+import boxes._
+import net.liftweb.common.Loggable
+import net.liftweb.http.SHtml
+import net.liftweb.http.js.JE
+import net.liftweb.http.js.JsCmd._
+import net.liftweb.http.js.JsCmds._
+import net.liftweb.json.DefaultFormats
+import net.liftweb.json.parse
 
 case class LinkDataIn(value: String, index: Long)
 case class LinkDataOut(value: String, index: Long, guid: String)
