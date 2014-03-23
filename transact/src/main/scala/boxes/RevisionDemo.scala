366c33c2fc42caf1ef91c75b7d60596719e88508
trepidacious <trepidacious@gmail.com>
2014-03-23 16:21:41 +0000

    Shorter form for transaction try in demo

--- transact/src/main/scala/boxes/RevisionDemo.scala
+++ transact/src/main/scala/boxes/RevisionDemo.scala
@@ -6,35 +6,23 @@ object RevisionDemo {
 
   def doInTry[T](f: (TransactionTry)=>T): T = {
     val r = Revision.first
-    implicit val t = new TransactionTryDefault(r)
+    val t = new TransactionTryDefault(r)
     f(t)
   }
-  
-  
-  
+
   def main(args: Array[String]): Unit = {
-//    val r = Revision.first
-//    implicit val t = new TransactionTryDefault(r)
-//    
-//    val a = Box("a")
-//    val b = Box("b")
-//    println("a = " + a())
-//    println("b = " + b())
-//    a() = "a2"
-//    println("a = " + a())
-//    println("b = " + b())
     
-    doInTry((t: TransactionTry) => {
-      implicit val t2 = t
-      val a = Box("a")
-      val b = Box("b")
-      println("a = " + a())
-      println("b = " + b())
-      a() = "a2"
-      println("a = " + a())
-      println("b = " + b())
+    doInTry {
+      implicit t: TransactionTry => {
+        val a = Box("a")
+        val b = Box("b")
+        println("a = " + a())
+        println("b = " + b())
+        a() = "a2"
+        println("a = " + a())
+        println("b = " + b())
       }
-    )
+    }
     
   }
 }
